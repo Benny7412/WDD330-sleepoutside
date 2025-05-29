1e81467b6f11c5e827258678f4ad4c77c653465c
Benny7412 <bennyklaus@gmail.com>
2025-05-29 04:45:10 -0500

    retry

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,11 @@
+
 import { getParam  } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
-const productID = getParam('product');
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource)
 product.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
