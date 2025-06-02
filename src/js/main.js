3561a787671f34450afc3cef938e51d45604abbb
Benny7412 <bennyklaus@gmail.com>
2025-06-01 21:26:36 -0500

    partials

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const productList = new ProductList();
 
+loadHeaderFooter();
